e4f6ebc07c900e7677ddfa3cf8946d5b24a95370
Cordre <cordrepayne@gmail.com>
2022-12-06 16:07:47 -0600

    Add auth context

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AuthProviderWrapper } from "./context/auth.context";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <AuthProviderWrapper>
+        <App />
+    </AuthProviderWrapper>
 );
