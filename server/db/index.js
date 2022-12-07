dbefc5599b8c5c6f57179ba589a54bf9497ea02d
Cordre <cordrepayne@gmail.com>
2022-12-07 13:06:30 -0600

    Change server location

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/server";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/test";
 
 mongoose
   .connect(MONGO_URI)
