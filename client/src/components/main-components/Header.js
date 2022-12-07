b7b948cfd900637a321e00f7765bbd9234f8ec84
Cordre <cordrepayne@gmail.com>
2022-12-06 18:03:27 -0600

    Add text for navbar links

--- client/src/components/main-components/Header.js
+++ client/src/components/main-components/Header.js
@@ -3,7 +3,20 @@ import "../../css/Header.css";
 export default function Header(){
     return(
         <nav>
-            header
+            <div>
+                <div>Logo</div>
+                <div>Home</div>
+                <div>TV Shows</div>
+                <div>Movies</div>
+                <div>Recently Added</div>
+                <div>My List</div>
+            </div>
+            <div>
+                <div>Search Icon</div>
+                <div>Kids Toggle</div>
+                <div>Notification Icon</div>
+                <div>Account icon<div>v</div></div>
+            </div>
         </nav>
     )
 }
